86f6102f54814505b1209a0f29bb0203d8d49142
Stella <jens300304@gmail.com>
2022-03-19 17:59:11 +0000

    flake.nix: add nix-gaming cache

--- flake.nix
+++ flake.nix
@@ -19,6 +19,11 @@
 	rust-overlay.url = "github:oxalica/rust-overlay";
     };
 
+  nixConfig = {
+    substituters = [ "https://cache.nixos.org" "https://nix-gaming.cachix.org" ];
+    trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+  };
+
     outputs = { self, nixpkgs, ... } @ inputs:
         let overlays = (import ./overlays)
 	    ++ [inputs.powercord-overlay.overlay]
