0ac8928186ff01fe53225adadc50fca59124f197
Stella <jens300304@gmail.com>
2022-03-19 15:23:20 +0000

    overlays/discord: add sha256sum

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -3,6 +3,7 @@ self: super: {
         version = "0.0.134";
 	src = builtins.fetchurl {
             url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
+	    sha256 = "0hynv9nh6lcfy44yxs33686iyzvjrnczxkppb75c6a4wa7m5l8hz";
 	};
     };
 }
