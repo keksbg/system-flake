38e0fef53059ec359ca5603fe84a249c6df76682
Stella <jens300304@gmail.com>
2022-03-20 03:06:56 +0200

    overlays/discord: add openasar

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,9 +1,16 @@
 self: super: {
-    discord-canary = super.discord-canary.override rec {
+    discord-canary-dl = (super.discord-canary.override rec {
         version = "0.0.134";
-	src = builtins.fetchurl {
+        src = builtins.fetchurl {
             url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-	    sha256 = "0hynv9nh6lcfy44yxs33686iyzvjrnczxkppb75c6a4wa7m5l8hz";
-	};
-    };
+            sha256 = "0hynv9nh6lcfy44yxs33686iyzvjrnczxkppb75c6a4wa7m5l8hz";
+        };
+    }).overrideAttrs (old: {
+        postFixup = (old.postFixup or "") + ''
+            cp ${super.fetchurl {
+                url = "https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar";
+                sha256 = "0r94674msglz1pwzbz9wzmn04sm57mfi3ayjnhswd14mjslclarl";
+            }} $out/opt/Discord/resources/app.asar
+        '';
+    });
 }
