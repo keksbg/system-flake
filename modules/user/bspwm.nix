26594889e5a0d187e5eb6f719a054da9b36b25f6
Stella <jens300304@gmail.com>
2022-03-20 02:39:46 +0200

    modules/user: make DP-0 the primary output

--- modules/user/bspwm.nix
+++ modules/user/bspwm.nix
@@ -16,7 +16,7 @@
     extraConfig = ''
       feh --no-fehbg --bg-fill ${config.xdg.configHome}/xorg/wallpaper.png
       systemctl --user restart polybar # home-manager loads this too early
-      xrandr --output DP-0 --mode 1920x1080 --refresh 144 --left-of HDMI-0
+      xrandr --output DP-0 --mode 1920x1080 --refresh 144 --primary --left-of HDMI-0
     '';
   };
 }
