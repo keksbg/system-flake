e338ee3bc0eabda1d46e064081bf800a2dbf6062
Stella <jens300304@gmail.com>
2022-03-28 19:53:43 +0300

    overlays/discord: update openasar hash

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -9,7 +9,7 @@ self: super: {
         postFixup = (old.postFixup or "") + ''
             cp ${super.fetchurl {
                 url = "https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar";
-                sha256 = "1llz6idxljnbwi8gm5hc6xci4f04wb62v2b8hlq61z44dicpcfd6";
+                sha256 = "0s564aq1hmik42xbb3li8i4hhm83fgajwfdhbbfgvhbksxf6576b";
             }} $out/opt/DiscordCanary/resources/app.asar
         '';
     });
