dc4c547990076cda8de61428ab531bab3b3ae45c
Stella <jens300304@gmail.com>
2022-04-16 13:24:37 +0200

    overlays/discord: update

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,9 +1,9 @@
 self: super: {
   discord-canary = (super.discord-canary.override rec {
-    version = "0.0.134";
+    version = "0.0.135";
     src = builtins.fetchurl {
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-      sha256 = "0hynv9nh6lcfy44yxs33686iyzvjrnczxkppb75c6a4wa7m5l8hz";
+      sha256 = "19ri82pwz43wma9kyy2109wdbrmq2zxy2bq03l3i7m4j2pfbwqbn";
     };
   });
 }
