956fe50c2b676c36912a37fbf331feac571c99d8
Stella <jens300304@gmail.com>
2022-06-28 21:38:31 +0200

    flake: update

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,9 +1,11 @@
 self: super: {
-  discord-canary = (super.discord-canary.override rec {
+  discord = (super.discord.override rec {
     version = "0.0.135";
     src = builtins.fetchurl {
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
       sha256 = "19ri82pwz43wma9kyy2109wdbrmq2zxy2bq03l3i7m4j2pfbwqbn";
     };
+  }).overrideAttrs (oldAttrs: {
+    withOpenASAR = true;
   });
 }
