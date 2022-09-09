74ad89be270550afc42222d3d3048d0c8b204694
Stella <jens300304@gmail.com>
2022-09-09 22:07:40 +0200

    replace discord-canary, not discord

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,10 +1,11 @@
 self: super: {
-  discord = (super.discord.override rec {
-    version = "0.0.135";
+  discord-canary = (super.discord.override rec {
+    version = "0.0.139";
     src = builtins.fetchurl {
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-      sha256 = "19ri82pwz43wma9kyy2109wdbrmq2zxy2bq03l3i7m4j2pfbwqbn";
+      sha256 = "1llafigs2zd49m212d49zi4klx39kn4r2msxz75cmi4i6p8wxxzw";
     };
+    binaryName = "DiscordCanary";
   }).overrideAttrs (oldAttrs: {
     withOpenASAR = true;
   });
